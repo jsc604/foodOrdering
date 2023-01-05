92833fb7dd778eba7363435217826d75f0ec5fa5
Bianca Fu <0509biancafu@gmail.com>
2023-01-04 21:12:11 -0700

    edited styling for cart

---
+++ db/queries/menu.js
@@ -0,0 +1,11 @@
+const db = require('../connection');
+
+const getItemOptions = function(item_name) {
+  return db.
+  query(`SELECT `)
+  .then(result => {
+    return 
+  })
+}
+
+module.exports = { getItemOptions };
