59820fc6f7938fdbb825c04f86f70e7cdd2d2240
Bianca Fu <0509biancafu@gmail.com>
2023-01-05 03:54:05 +0000

    added new sample data

--- db/seeds/01_menu_category.sql
+++ db/seeds/01_menu_category.sql
@@ -1,4 +1,11 @@
-INSERT INTO menu_category (name) VALUES ('test category 1');
-INSERT INTO menu_category (name) VALUES ('test category 2');
-INSERT INTO menu_category (name) VALUES ('test category 3');
-INSERT INTO menu_category (name) VALUES ('test category 4');
+-- INSERT INTO menu_category (name) VALUES ('test category 1');
+-- INSERT INTO menu_category (name) VALUES ('test category 2');
+-- INSERT INTO menu_category (name) VALUES ('test category 3');
+-- INSERT INTO menu_category (name) VALUES ('test category 4');
+INSERT INTO menu_category (name) VALUES ('Coffee');
+INSERT INTO menu_category (name) VALUES ('Ice Coffee');
+INSERT INTO menu_category (name) VALUES ('Tea');
+INSERT INTO menu_category (name) VALUES ('Food');
+INSERT INTO menu_category (name) VALUES ('Pastry');
+
+
