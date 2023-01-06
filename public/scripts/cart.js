add96e4f8fea2427801155819adabb62c69e7d22
Bianca Fu <0509biancafu@gmail.com>
2023-01-05 23:11:50 -0700

    edited functions so we get correct info on cart

---
+++ public/scripts/cart.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  $('.confirm-order').submit(function(event) {
+    event.preventDefault();
+    const orderItems = [];
+    const orderItemElements = $(this).parent().parent().siblings().find('.cart-order-item');
+    for (let item of orderItemElements) {
+      const quantity = $(item).find('.quantity').text();
+      const name = $(item).find('.item-name').text();
+      const price = $(item).find('.price').text();
+      const instruction = $(item).find('.instruction').text();
+      orderItems.push({quantity, name, price, instruction});
+    }
+    console.log(orderItems);
+  })
+
+  $.ajax({
+    url:"/confirm-order",
+    method: "POST",
+    data: $(this).serialize(),
+    success: function (){
+        alert ("hello");
+    },
+    error: function(res) {
+        alert(res)
+    }
+})
+})
+
