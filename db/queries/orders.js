12a805041bcc80e648df5a799ef1f16e593935ad
Jeffrey Cheung <jeffreycheung_@live.com>
2023-01-05 16:31:15 -0800

    edited queries and routes to accomodate new erd table and seeds

--- db/queries/orders.js
+++ db/queries/orders.js
@@ -3,15 +3,14 @@ const db = require('../connection');
 // GET NEW ORDERS
 const getNewOrders = () => {
   return db.query(`
-  SELECT o.id AS order_id, c.name AS customer_name, i.name AS item_name,
-       a.name AS item_addon, op.name AS item_option, o.start_time, o.completion_time AS completed_at
+  SELECT o.id AS order_id, c.name AS customer_name, i.name AS item_name, mc.id AS category_id,
+       oi.size, oi.milk, oi.espresso_option, oi.flavour, oi.special_request, oi.quantity, o.start_time
   FROM orders o
   JOIN customers c ON o.customer_id = c.id
   JOIN order_items oi ON o.id = oi.order_id
   JOIN menu_items i ON oi.item_id = i.id
-  LEFT JOIN menu_item_addons a ON oi.item_addon_id = a.id
-  LEFT JOIN menu_item_options op ON oi.item_option_id = op.id
-  WHERE status = 'incomplete';
+  JOIN menu_category mc ON i.category_id = mc.id
+  WHERE o.status = 'incomplete';
   `)
     .then(data => {
       return data.rows;
@@ -21,16 +20,16 @@ const getNewOrders = () => {
 // GET COMPLETED ORDERS
 const getCompletedOrders = () => {
   return db.query(`
-  SELECT o.id AS order_id, c.name AS customer_name, i.name AS item_name,
-       a.name AS item_addon, op.name AS item_option, o.completion_time AS completed_at, o.picked_up
-  FROM orders o
-  JOIN customers c ON o.customer_id = c.id
-  JOIN order_items oi ON o.id = oi.order_id
-  JOIN menu_items i ON oi.item_id = i.id
-  LEFT JOIN menu_item_addons a ON oi.item_addon_id = a.id
-  LEFT JOIN menu_item_options op ON oi.item_option_id = op.id
-  WHERE o.status = 'complete';
-  `)
+  SELECT o.id AS order_id, c.name AS customer_name, i.name AS item_name, mc.id AS category_id,
+  oi.size, oi.milk, oi.espresso_option, oi.flavour, oi.special_request, oi.quantity,
+  o.completion_time AS completed_at, o.picked_up
+FROM orders o
+JOIN customers c ON o.customer_id = c.id
+JOIN order_items oi ON o.id = oi.order_id
+JOIN menu_items i ON oi.item_id = i.id
+JOIN menu_category mc ON i.category_id = mc.id
+WHERE o.status = 'complete';
+    `)
     .then(data => {
       return data.rows;
     });
