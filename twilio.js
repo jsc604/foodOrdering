45bf4ff98e6953a1be19fda523f7ab1c0c47cfe4
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-01-09 14:52:17 -0700

    up to date

--- twilio.js
+++ twilio.js
@@ -7,14 +7,6 @@ const toNumber = process.env.toNumber;
 const fromNumber = process.env.fromNumber;
 const client = require("twilio")(accountSid, authToken);
 
-// client.messages
-// .create({
-//   body: 'hello world test',
-//   from: fromNumber,
-//   to: toNumber
-// })
-// .then((message) => console.log(message));
-
 const messageToCustomer = (name, orderID, waitTime, phone) => {
   client.messages
     .create({
